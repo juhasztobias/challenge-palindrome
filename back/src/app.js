3b91ca6a8868b7d74c9567ab3874bef4010540a9
Lisandro <salvareschilisandro0@gmail.com>
2024-12-17 12:49:40 -0300

    add route in app.js

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { json } from 'express';
+import { sanitizeWord } from './utilities/sanitize-words.js';
+
 dotenv.config();
 
 export const app = express();
@@ -9,7 +11,7 @@ app.use(json());
 
 /**
  * Routes for the challenge
- */
+**/
 
 // GET /ping
 app.get('/ping', (req, res) => {
@@ -17,3 +19,22 @@ app.get('/ping', (req, res) => {
     "message": 'pong',
   });
 });
+
+// Endpoint para determinar si una palabra es palindromo o no
+// POST /palindrome
+app.post('/palindrome', (req, res) => {
+  // Recibo la palabra a través del body de la request
+  const { word } = req.body;
+
+  const sanitizedWord = sanitizeWord(word);
+
+  // Con .split convertimos el string en un array para hacer uso del método .reverse()
+  // Este metodo reverse, devuelve el array invertido y por último volvemos a convertir el array en un string
+  const reverseWord = sanitizedWord.split("").reverse().join("");
+
+  return res.json({
+    word,
+    // Devuelve true si el string sanitizado es igual al string reversado, es decir, la palabra es polindromo
+    isPolindrome: reverseWord === sanitizedWord
+  })
+})
