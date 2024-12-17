2768071feb347639f6a8cbbbba2e41e4daa5f7ce
LeoSebastian23 <leosebastiang19@gmail.com>
2024-12-16 21:37:47 -0300

    Update history.js

--- back/src/models/history.js
+++ back/src/models/history.js
@@ -1,4 +1,4 @@
 // Array para almacenar el historial
 export const history = []; 
-/* Primeramente hacemos la prueba en un Array, de funcionar luego podemos implementar
+/* Primeramente hacemos la prueba en un Array, luego podemos implementar
 la persistencia del historial utilizando una base de datos.*/
